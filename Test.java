339c2d8e446222919fd553ce8f4e8b32b79db9d7
Nikhitab1 <b.nikhitha.sai@gmail.com>
2016-12-16 20:57:01 +0530

    Add files via upload
    Compiles and prints the output 10.

---
+++ Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+	
+	 public static void main(String[] args){
+	 }
+	 
+	static{
+		print(10);
+		}
+		static void print (int x) {
+		System.out.println (x);
+		System.exit(0);
+
+}
+	 
+}
+
